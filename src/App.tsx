4a99dc9f37ff27aa8648c6c7013bfacdffde3f70
Pooriadevx <pooria.b1378@gmail.com>
2024-06-07 22:00:09 +0330

    feat: Added components with refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MoviesList from "./components/moviesList/moviesList";
+import Filter from "./components/filter/filter";
+import {
+  FilteredListType,
+  FilterValues,
+  HandleFilterType,
+  MovieCardType,
+} from "./types/moviesList";
+import {
+  SelectGenreOptions,
+  SelectRateOptions,
+} from "./constants/filterOptions";
+import { filteredBaseGenre, filteredBaseRate } from "./utils/filterList";
+import data from "./data.json";
+import classes from "./App.module.scss";
 
 function App() {
+  const [moviesList, setMoviesList] = useState<MovieCardType[]>(data);
+  const [searchParams] = useSearchParams();
+
+  const handleFilteredList: HandleFilterType = (value, type) => {
+    if (value) {
+      const obj: FilteredListType = {
+        [FilterValues.Rate]: () => filteredBaseRate(value, moviesList),
+        [FilterValues.Genre]: () => filteredBaseGenre(value, data),
+      };
+      return obj[type as keyof FilteredListType]();
+    } else {
+      return data;
+    }
+  };
+
+  const handleInitialData = () => {
+    let dataOfList: MovieCardType[] = data;
+
+    if (searchParams.size) {
+      searchParams.forEach((value, key) => {
+        dataOfList = handleFilteredList(value, key);
+        console.log(dataOfList);
+      });
+    }
+    setMoviesList(dataOfList);
+  };
+
+  useEffect(() => handleInitialData(), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classes.App}>
+      <header className={classes.App_header}>
+        <div className={classes.App_header_filter}>
+          <Filter
+            handleFilter={(value, type) =>
+              setMoviesList(handleFilteredList(value, type))
+            }
+            multiSelectOptions={SelectGenreOptions}
+          />
+          <Filter
+            handleFilter={(value, type) => {
+              setMoviesList(handleFilteredList(value, type));
+            }}
+            multiSelectOptions={SelectRateOptions}
+          />
+        </div>
+        <MoviesList moviesList={moviesList} />
       </header>
     </div>
   );
